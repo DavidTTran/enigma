9ff483c82132577d3464d08f2b3565c2fdc0846c
David Tran <davidttemail@gmail.com>
2020-02-28 11:59:03 -0700

    Add method to convert key to pairs

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,12 +4,18 @@ class Enigma
   end
 
   def encrypt(message, key, date)
-    get_info(key, date)[:encrypt]
+    key_to_pairs(key)
 
     encryption_info = {}
     encryption_info[:encryption] = new_message(message.downcase)
     encryption_info[:key] = key
     encryption_info[:date] = date
     encryption_info
+  end
+
+  def key_to_pairs(key)
+    keys = []
+    key = key.split("").each_cons(2) { |pair| keys << pair }
+    keys.map { |key| key.join.to_i }
   end
 end

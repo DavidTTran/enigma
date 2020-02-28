0649b144816fda673d0ded4cf1192e0a365a733f
David Tran <davidttemail@gmail.com>
2020-02-28 12:41:17 -0700

    Add methods to decrypt message

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -25,10 +25,18 @@ class Enigma
   end
 
   def shift_alphabet(function = "encrypt")
-    offset1 = @alphabet.rotate(@offset_values[0]).join
-    offset2 = @alphabet.rotate(@offset_values[1]).join
-    offset3 = @alphabet.rotate(@offset_values[2]).join
-    offset4 = @alphabet.rotate(@offset_values[3]).join
+    if function == "decrypt"
+      @offset_values = @offset_values.map { |value| value * -1 }
+      offset1 = @alphabet.rotate(@offset_values[0]).join
+      offset2 = @alphabet.rotate(@offset_values[1]).join
+      offset3 = @alphabet.rotate(@offset_values[2]).join
+      offset4 = @alphabet.rotate(@offset_values[3]).join
+    else
+      offset1 = @alphabet.rotate(@offset_values[0]).join
+      offset2 = @alphabet.rotate(@offset_values[1]).join
+      offset3 = @alphabet.rotate(@offset_values[2]).join
+      offset4 = @alphabet.rotate(@offset_values[3]).join
+    end
     @offset = [offset1, offset2, offset3, offset4]
   end
 
@@ -43,6 +51,17 @@ class Enigma
       end
     end
     new_message
+  end
+
+  def decrypt(encrypted_message, key, date)
+    get_info(key, date)
+    shift_alphabet("decrypt")
+
+    decryption_info = {}
+    decryption_info[:decryption] = new_message(encrypted_message.downcase)
+    decryption_info[:key] = key
+    decryption_info[:date] = date
+    decryption_info
   end
 
 end

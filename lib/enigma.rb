e7d1a638c28f09281b5844f05b2150d94a5ec9a1
David Tran <davidttemail@gmail.com>
2020-02-28 11:52:38 -0700

    Write method for encrypt return value

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,4 +2,14 @@ class Enigma
 
   def initialize
   end
+
+  def encrypt(message, key, date)
+    get_info(key, date)[:encrypt]
+
+    encryption_info = {}
+    encryption_info[:encryption] = new_message(message.downcase)
+    encryption_info[:key] = key
+    encryption_info[:date] = date
+    encryption_info
+  end
 end
